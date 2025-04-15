f620d36ec3c7717ebf22dbc7d4daf5fe995d4f43
reflectivemind247 <reflectivemind@proton.me>
2025-04-15 13:05:15 +0530

    Update blogrss.js

--- assets/blogrss.js
+++ assets/blogrss.js
@@ -5,7 +5,7 @@
         document.getElementById("viewDate").innerText = buildDate(item);
         document.getElementById("viewTitle").innerText = item.title;
         document.getElementById("viewContent").innerHTML = item.content;
-//        document.getElementById("viewImage").src = item.querySelector("imageurl").textContent;
+        document.getElementById("viewImage").src = item.querySelector("imageurl").textContent;
         document.getElementById("postView").classList.remove("hidden");
         document.getElementById("rss-feed").classList.add("hidden");
     }
